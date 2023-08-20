29d2073ecead2ff0a77f66500ec54901b3dc725a
James Kelly <jk377y@gmail.com>
2023-08-20 05:20:18 -0500

    minor code formatting changes

--- README.md
+++ README.md
@@ -1,23 +1,29 @@
 # Sticky Notes App
+
 [![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT)<br>
 
 ## Live Application
+
 https://sticky-notes-iota.vercel.app/
 
 ## Description
+
 This is a quick Javascript application that I am using to mimic the behavior of the Windows Sticky Note application. Notes are displayed on the page and also are persistent through using the browser's local storage.<br>
 
 ## Screenshot
+
 <img src="./screenshot1.JPG" alt="screenshot of application being used in browser"><br>
 
-## *Questions*
+## _Questions_
+
 <h3>Portfolio:&emsp;<a href="https://jk377y.com" target="_blank">https://jk377y.com</a></h3>
 <h3>Email:&emsp;<a href="mailto:jk377y@gmail.com" target="_blank">jk377y@gmail.com</a></h3>
 <h3>LinkedIn:&emsp;<a href="https://www.linkedin.com/in/james-kelly-software-developer/" target="_blank">https://www.linkedin.com/in/james-kelly-software-developer/</a></h3>
 <h3>GitHub:&emsp;<a href="https://github.com/jk377y" target="_blank">https://github.com/jk377y</a></h3>
 <br>
 
-## *License*
+## _License_
+
 [![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT)
 <br>Copyright (c) 2023 James Kelly
 <br>Information on this license can be found at: (https://opensource.org/licenses/MIT)
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
- // start by declaring the needed elements from the html file
+// start by declaring the needed elements from the html file
 const startTitleInput = document.getElementById('startTitle');
 const startTextInput = document.getElementById('startText');
 const addNoteBtn = document.querySelector('.addNoteBtn');
@@ -6,19 +6,20 @@ const container = document.querySelector('.container');
 
 // when the page loads i want to load any existing notes from local storage and display them on the page
 window.addEventListener('load', () => {
-    // "getting" any notes stored in local storage and mapping the notes to the container using the createNoteElement function
+	// "getting" any notes stored in local storage and mapping the notes to the container using the createNoteElement function
 	const notes = JSON.parse(localStorage.getItem('notes')) || [];
 	notes.map((noteData) => {
-        const note = createNoteElement(noteData.title, noteData.text); // this function returns the note element
-        container.appendChild(note);  // adds the note to the container
-      });
+		const note = createNoteElement(noteData.title, noteData.text); // this function returns the note element
+		container.appendChild(note); // adds the note to the container
+	});
 });
 
 addNoteBtn.addEventListener('click', () => {
-    // need to get the values of the input fields
+	// need to get the values of the input fields
 	const title = startTitleInput.value;
 	const text = startTextInput.value;
-	if (title || text) { // if either of the fields are filled out it allows the note to be created
+	if (title || text) {
+		// if either of the fields are filled out it allows the note to be created
 		const note = createNoteElement(title, text);
 		container.appendChild(note); // adds the new note to the container
 		saveNoteToLocalStorage(title, text); // uses the saveNoteToLocalStorage function to save the note to local storage
@@ -29,15 +30,15 @@ addNoteBtn.addEventListener('click', () => {
 
 // this function creates the note element
 const createNoteElement = (title, text) => {
-    // creates the div with a class of note and adds the title and text to it from the input fields declared above using template literals
-	const note = document.createElement('div'); 
+	// creates the div with a class of note and adds the title and text to it from the input fields declared above using template literals
+	const note = document.createElement('div');
 	note.classList.add('note');
 	note.innerHTML = `<h2>${title}</h2><p>${text}</p>`;
-    // creates the delete button and adds a class to it for styling purposes and adds text to it
+	// creates the delete button and adds a class to it for styling purposes and adds text to it
 	const deleteNoteBtn = document.createElement('button');
 	deleteNoteBtn.classList.add('deleteNoteBtn');
 	deleteNoteBtn.textContent = 'Delete Note';
-    // adds an event listener to the delete button that removes the note from the container and local storage
+	// adds an event listener to the delete button that removes the note from the container and local storage
 	deleteNoteBtn.addEventListener('click', () => {
 		container.removeChild(note);
 		removeNoteFromLocalStorage(title, text);
